1fa50081ee8f76c666ca33d33e72674c01cc79a2
SOUROV KHAN NAHID <67148243+SOUROVKHANNAHID@users.noreply.github.com>
2023-07-05 01:24:10 +0600

    Update and rename omni_Infinix-X6812.mk to twrp_X6812.mk

---
+++ twrp_X6812.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from X6812 device
+$(call inherit-product, device/infinix/X6812/device.mk)
+
+PRODUCT_DEVICE := X6812
+PRODUCT_NAME := twrp_X6812
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix HOT 11S
+PRODUCT_MANUFACTURER := infinix
