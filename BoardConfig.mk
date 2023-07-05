f1a586aeae8bd448b87e754a1a0ee6af41984d4e
SOUROV KHAN NAHID <67148243+SOUROVKHANNAHID@users.noreply.github.com>
2023-07-05 13:27:55 +0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,14 +12,7 @@ ALLOW_MISSING_DEPENDENCIES := true
 
 # A/B
 AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    system \
-    system_ext \
-    vendor \
-    product \
-    boot \
-    vbmeta_vendor \
-    vbmeta_system
+
 BOARD_USES_RECOVERY_AS_BOOT := true
 
 # Architecture
